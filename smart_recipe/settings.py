d349bf37b3d76217a7306d2c8d568b4890a688c9
AnaKostovska12 <kostovskaana360@gmail.com>
2025-09-14 17:27:51 +0200

    Configure WhiteNoise and fix static files path

--- smart_recipe/settings.py
+++ smart_recipe/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -57,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 ROOT_URLCONF = 'smart_recipe.urls'
 
@@ -126,8 +128,11 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
-    BASE_DIR / "recipes" / "static_recipes",
+    BASE_DIR / "recipes" / "static",  # point to your existing static folder
 ]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "recipes" / "static_recipes",
+# ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
